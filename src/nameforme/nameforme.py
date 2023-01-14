5b6ebf94ad2be864745e9ea4732278a3e0bdd290
Zilong Yi <73105919+ZilongYi@users.noreply.github.com>
2023-01-13 18:33:26 -0800

    documentation for the last two functions

--- src/nameforme/nameforme.py
+++ src/nameforme/nameforme.py
@@ -21,7 +21,7 @@ def find_unisex_name(limit=0.2):
 def find_old_name(tp,limit=10, sex="uni"):
     """
     Generate the a random set of 10 suggested neutral(by default) baby names 
-    based on the given time period and .
+    based on the given time period and sex.
     Parameters
     ----------
     tp : string, default 10
@@ -38,5 +38,48 @@ def find_old_name(tp,limit=10, sex="uni"):
     --------
     >>> find_old_name('80s')
     >>> ['Skylar', 'Azariah', 'Royal', 'Hayden', 'Emerson', 'Rowan', 'Baylor', 'Dakota', 'River', 'Emory']
+    """
+    pass
+
+def find_similar_name(name, limit=10):
+    """
+    Generate the a random set of 10 suggested similar baby names 
+    based on the syllable of the input name. 
+    Parameters
+    ----------
+    name : string
+        the given name used to find similar names based on syllable. 
+    limit : float
+        The number of name in the output list.  
+    Returns
+    -------
+    list:
+        A name list containing random suggested similar names based on the given name.
+    Examples
+    --------
+    >>> find_old_name('Rowan')
+    >>> ['Skylar', 'Azariah', 'Royal', 'Hayden', 'Emerson', 'Rowan', 'Baylor', 'Dakota', 'River', 'Emory']
+    """
+    pass
+
+def find_name(sex, init, len):
+    """
+    Generate the a random set of 10 suggested baby names based on the given limitations.
+    Parameters
+    ----------
+    sex: string, 
+        The sex of baby's name 
+    init : string
+        The initial of baby's name
+    len : int
+        The length of baby's name 
+    Returns
+    -------
+    list:
+        A name list containing random suggested names based on the given limitation.
+    Examples
+    --------
+    >>> find_name('boy', 'A', 3)
+    >>> ['Aiden', 'Aaron', 'Asher','Angel','Adam']
     """
     pass
