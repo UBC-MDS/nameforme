17f4a96657f58ac0d40dafde93d27f3595646a1d
Daniel Cairns <daniel@cairns.ca>
2023-01-16 14:36:38 -0800

    Wrote unit tests for find_similar_name function

---
+++ tests/test_find_similar_name.py
@@ -0,0 +1,18 @@
+# Authous: Daniel Cairns, Eyre Hong, Bruce Wu, Zilong Yi (UBC MDS)
+# Date: Jan 16th, 2023
+
+from nameforme.nameforme import find_similar_name
+
+def test_output_length():
+    """Test that 10 names are output"""
+    assert len(find_similar_name('Daniel', limit=3)) == 3
+    
+def test_names_are_distinct():
+    """Test that there are no duplicate names in output"""
+    assert len(set(find_similar_name('Daniel'))) == 10
+    
+def test_names_are_random():
+    """Test that repeated calls generate different names"""
+    first_call = find_similar_name('Daniel', limit=20)
+    second_call = find_similar_name('Daniel', limit=20)
+    assert set(first_call) != set(second_call)
