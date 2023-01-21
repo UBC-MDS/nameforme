e6de2d93557ddf6d003711f02e337ce946ddd0ae
Zilong Yi <73105919+ZilongYi@users.noreply.github.com>
2023-01-21 12:08:17 -0800

    Adding error messages.

--- tests/test_find_old_name.py
+++ tests/test_find_old_name.py
@@ -7,17 +7,17 @@ def test_find_old_name():
     """Test cases for find_old_name()"""
 
     """Test that n names are output"""
-    assert len(find_old_name('1980s', limit=3)) == 3
+    assert len(find_old_name('1980s', limit=3)) == 3, 'The list provides different length of names'
     
     """Test that there are no duplicate names in output"""
-    assert len(set(find_old_name('1980s'))) == 10
+    assert len(set(find_old_name('1980s'))) == 10, 'The defualt list is not of length 10'
     
     """Test that repeated calls generate different names"""
     first_call = find_old_name('1980s', limit=10)
     second_call = find_old_name('1980s', limit=10)
-    assert set(first_call) != set(second_call)
+    assert set(first_call) != set(second_call), 'The list generated is not random'
 
     """Test that repeated calls with seed given generate same names"""
     first_call = find_old_name('1980s', seed=123)
     second_call = find_old_name('1980s', seed=123)
-    assert set(first_call) != set(second_call)
+    assert set(first_call) != set(second_call), 'The lists generated with given seed are not the same.'
