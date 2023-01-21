6808ce00bf4f02ea84742989f508be2212143463
DanielCairns <35279544+DanielCairns@users.noreply.github.com>
2023-01-20 18:01:51 -0800

    Added one test for similarity

--- tests/test_find_similar_name.py
+++ tests/test_find_similar_name.py
@@ -16,3 +16,8 @@ def test_names_are_random():
     first_call = find_similar_name('Daniel', limit=20)
     second_call = find_similar_name('Daniel', limit=20)
     assert set(first_call) != set(second_call)
+
+def test_names_seem_similar():
+    """Crude similarity test checking if first letter matches"""
+    assert "D" in [name[0] for name in find_similar_name('Daniel', 3)]
+    
